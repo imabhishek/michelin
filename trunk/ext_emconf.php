2e5246a0aa5e1ad28f36d93ccdf2066170814b87
funksyfunks <funksyfunks@7f129c00-99d6-11dd-9929-29c1920dd198>
2008-10-14 11:08:08 +0000

    Initial import

---
+++ trunk/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "tcproject"
+#
+# Auto generated 14-10-2008 10:53
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Project Space',
+	'description' => 'Project Space',
+	'category' => 'module',
+	'author' => 'Claus Bruun',
+	'author_email' => 'cb@typoconsult.dk',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => 'mod1',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"e59f";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"4b4c";s:19:"doc/wizard_form.dat";s:4:"4ffa";s:20:"doc/wizard_form.html";s:4:"6e26";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"feec";s:14:"mod1/index.php";s:4:"cee3";s:18:"mod1/locallang.xml";s:4:"a484";s:22:"mod1/locallang_mod.xml";s:4:"5f96";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
+);
+
+?>
