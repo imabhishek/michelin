55208ef30a87e309e178383ece9dc31c519ff315
funksyfunks <funksyfunks@7f129c00-99d6-11dd-9929-29c1920dd198>
2008-10-15 11:37:18 +0000

    Added pi1

--- tcfilearchive/ext_emconf.php
+++ tcfilearchive/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "tcfilearchive"
 #
-# Auto generated 15-10-2008 12:30
+# Auto generated 15-10-2008 13:36
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"108e";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"8e4b";s:14:"ext_tables.sql";s:4:"449b";s:33:"icon_tx_tcfilearchive_content.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"848e";s:7:"tca.php";s:4:"9df1";s:19:"doc/wizard_form.dat";s:4:"71c3";s:20:"doc/wizard_form.html";s:4:"ba13";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"108e";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"2611";s:14:"ext_tables.php";s:4:"21c3";s:14:"ext_tables.sql";s:4:"449b";s:33:"icon_tx_tcfilearchive_content.gif";s:4:"475a";s:13:"locallang.xml";s:4:"96a6";s:16:"locallang_db.xml";s:4:"ffe8";s:7:"tca.php";s:4:"9df1";s:19:"doc/wizard_form.dat";s:4:"f763";s:20:"doc/wizard_form.html";s:4:"09ed";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:34:"pi1/class.tx_tcfilearchive_pi1.php";s:4:"e134";s:42:"pi1/class.tx_tcfilearchive_pi1_wizicon.php";s:4:"ec17";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"9ca5";s:24:"pi1/static/editorcfg.txt";s:4:"c8e0";}',
 );
 
 ?>
--- tcfilearchive/ext_tables.php
+++ tcfilearchive/ext_tables.php
@@ -16,4 +16,17 @@ $TCA["tx_tcfilearchive_content"] = array (
 		"fe_admin_fieldList" => "title, type, path",
 	)
 );
+
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
+
+
+t3lib_extMgm::addPlugin(array('LLL:EXT:tcfilearchive/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
+
+
+t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","Filearchive");
+
+
+if (TYPO3_MODE=="BE")	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_tcfilearchive_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_tcfilearchive_pi1_wizicon.php';
 ?>
